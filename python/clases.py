a4820abc9e7e0e9fa4ea7d7b2c46b8cc9d298c1f
David Moranchel <david.arcshtag@gmail.com>
2016-04-27 22:27:27 -0500

    add clases.py

---
+++ python/clases.py
@@ -0,0 +1,17 @@
+class calculadora():
+	def suma(x,y):
+		print('La suma es : ',x + y)
+
+	def resta(x,y):
+		print('La resta es : ',x - y)
+
+	def multi(x,y):
+		print('La multiplicación es : ',x * y)
+
+	def div(x,y):
+		print('La división es : ',x / y)
+
+calculadora.suma(2,3)
+calculadora.resta(2,3)
+calculadora.multi(2,3)
+calculadora.div(2,3)
